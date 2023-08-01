5939c2ed3b0c98c9716bd17d80520c11db5dd276
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-08-01 12:37:17 +0530

    Crud operations with MONGOOSE by conncting mongodb

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./models/user");
+// const mongoConnect = require("./util/database").mongoConnect;
+// const User = require("./models/user");
 
 const app = express();
 
@@ -18,21 +19,32 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("64c78016da07bf4680b47b83")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-  // next();
-});
+// app.use((req, res, next) => {
+//   User.findById("64c78016da07bf4680b47b83")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+//   // next();
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+// mongoConnect(() => {
+//   app.listen(3000);
+// });
+
+mongoose
+  .connect(
+    "mongodb+srv://Jay:2pIFb1cyLWvESxJ0@cluster0.uajpbo9.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
